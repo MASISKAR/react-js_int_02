8279d4c32f364def05451492455351dc3bacc345
Masis <masiskar@gmail.com>
2020-12-14 21:30:12 +0400

    understanding functional components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+function Greeting(){
+
+  const name = "Alex";
+
+  return (
+    <div>
+    <h4>Hello, I am a function component!!!</h4>
+    <h3>Hello, I am {name}!</h3>
+    </div>
+  );
+}
+
+function Name(props){
+  return (
+    <span>{props.name}</span>
+  );
+}
+
+function Surname(props){
+  return (
+    <span>{props.surname}</span>
+  );
+}
+
+function User(props){
+  // let surname;
+  // if(props.surname){
+  //   surname = props.surname;
+  // }
+  // else{
+  //   surname='no surname';
+  // }
+
+  return (
+    <div >
+    <h3>Hello, I am <Name name={props.name}/> <Surname surname={props.surname || 'no surname'}/>, and I am {props.age}!</h3>
+    <a href = {props.href}>Go to Google</a>
+    </div>
+
+  );
+}
+
+
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className={"App-header"}>
+      
+        <Greeting />
+
+        <User 
+        name='Alex' 
+        age={25}
+        surname='Doe'
+        />
+
+        <User 
+        name='John' 
+        age={38}
+        surname='Smith'
+        />
+
+        <User 
+        name='Sarah' 
+        age={46} 
+        href='https://google.com'
+        />
       </header>
     </div>
   );
